f28413394233634523566a8cff85373cd8dbfc29
danielmk <danielkup3@gmail.com>
2022-01-23 21:00:40 +0200

    final version to send to the partner

--- hw3_part2.py
+++ hw3_part2.py
@@ -1,14 +1,14 @@
-def get_palindrom_dict(str):
-    palindrom_dict = {}
-    palindrom_dict[1] = [str[j] for j in len(str)]
-    palindrom_dict[2] = [str[i:i+2] for i in range(len(str)-1) if str[i] == str[i+1]]
-    even = [ i  for i in range(len(str)) if (i >= 4 and i % 2 == 0)]
-    for i in even:
-        polindroms = []
-        for j in range(len(str)-i):
-            if (str[j: j+(i/2)+1] == str[j+(i/2): j+i+1]):
-                polindroms.append(str[j:j+i+1])
-        palindrom_dict[i] = polindroms
-    return palindrom_dict
+def get_pal_new(str):
+    dc = {num + 1: [str[i:i + num + 1] for i in range(len(str) - num)
+                    if str[i:i + num + 1] == str[i:i + num + 1][::-1]] for num in range(len(str))}
+    return {num + 1: dc[num + 1] for num in range(len(str)) if len(dc[num+1]) > 0}
 
 def check_match(str):
+    if(len(str) ==0):
+        return True
+    fir_str = str[1::2]
+    sec_str = str[2::2]
+    for i in range(len(fir_str)):
+        if(fir_str.replace(fir_str[i],sec_str[i]) == sec_str):
+            return True
+    return False
